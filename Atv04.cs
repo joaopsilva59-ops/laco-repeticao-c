408c3f3859416a9c0719f11fd7e866027bff5fc3
JMR_996 <mateusjoseph@gmail.com>
2026-04-10 22:06:14 -0300

    feat: Atv04.cs
    Inserindo enunciado

---
+++ Atv04.cs
@@ -0,0 +1,16 @@
+/*
+Desenvolva um programa em C# que permita ao usuário escolher um número para gerar a sua tabuada.
+
+O programa deve:
+-solicitar ao usuário o número desejado;
+-calcular e exibir a tabuada desse número;
+
+Regras da atividade
+utilizar estrutura de repetição para exibir os cálculos;
+apresentar cada operação de forma organizada;
+mostrar a tabuada completa do número informado pelo usuário.
+
+Observação: Vestigios de uso de IA, atividade zerada, 
+ou código copiado de terceiros sem a devida citação, serão penalizados.
+
+*/
